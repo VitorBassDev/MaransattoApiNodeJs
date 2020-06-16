222635704d9f686e1bc22b7a09d918aa5a0082a5
Vitor Dev <vittorbassdev@gmail.com>
2020-06-16 20:57:59 -0300

    TRATAR ERROS | TRATAR ERROS DE ROTAS(QUANDO A ROTA NÃO FOR ENCONTRADA)

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const express = require ('express');
-const { response } = require('express');
+//const { response } = require('express');
 const app = express();
+// MONITORAR LOGS
+const morgan = require('morgan');
 
 // CRIAR ROTA
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require ('./routes/pedidos');
 
+//SETAR LOGS
+app.use(morgan('dev'));
+
 // SETAR ROTA PARA PRODUTOS
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
+
+// TRATAR ERROS | TRATAR ERROS DE ROTAS(QUANDO A ROTA NÃO FOR ENCONTRADA)
+app.use((request, response, next) =>{
+    const erro = new Error('Operação não encontrada');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, request, response, next) =>{
+    response.status(error.status || 500);
+    return response.send({
+        erro: {
+        mensagem: error.message
+        }
+    });
+});
 
 module.exports = app;
 
--- server.js
+++ server.js
@@ -4,3 +4,4 @@ const port = process.env.PORT || 4343;
 const server = http.createServer(app);
 server.listen(port);
 console.log('Executando na porta 4343');
+// INSTALAÇÃO DO NOODEMOON - npm install --save-dev nodemon
