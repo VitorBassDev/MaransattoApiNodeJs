4871ad0809de7fcf1d02dc4a95667cc4928043d4
VitorBassDev <vittorbassdev@gmail.com>
2020-06-24 20:37:57 -0300

    CRUD PRODUTOS| Refatoração - Produtos Implementação do Crud Refatoração do Código

--- routes/produtos.js
+++ routes/produtos.js
@@ -4,21 +4,26 @@ const router = express.Router();
 /**
  * CRIAÇÃO DAS ROTAS - PRODUTOS 
  * GET | POST | PATCH | DELETE 
+ * req.connection ( 
+ *                  A COXEXÃO É ABERTA ATRAVÉS DA ROTA
+ *                  ABRIR CONEXÃO COM O BANCO DE DADOS
+ *                  CONEXÃO É DEVOLVIDA AO POOL E ENCERRADA ATRAVES DO MIDDLWARE
+ *                )
  */
 
  /**
  * ROTA GET(BUSCAR)
  */
 router.get('/', (req, res, next) => {
-
-    // A COXEXÃO É ABERTA ATRAVÉS DA ROTA
-    // ABRIR CONEXÃO COM O BANCO DE DADOS
-    req.connection.query('SELECT * FROM produtos', (err, resultado) => {
-        if(err) return next(err);
-        return res.json({
-            produtos: resultado
-        });  
-        // CONEXÃO É DEVOLVIDA AO POOL E ENCERRADA ATRAVES DO MIDDLWARE
+    req.connection.query(
+        'SELECT * FROM produtos',
+            (err, resultado) => 
+            {
+                if(err) 
+                    return next(err);
+                        return res.json({
+                            produtos: resultado
+                        });  
     });
 });
      
@@ -26,84 +31,76 @@ router.get('/', (req, res, next) => {
  * ROTA POST(INSERIR)
  */
 router.post('/', (req , res, next) =>{
-    const produto = {
+    const produto = 
+    {
         nome: req.body.nome,
         preco: req.body.preco
     }
-    // A COXEXÃO É ABERTA ATRAVÉS DA ROTA
-    // ABRIR CONEXÃO COM O BANCO DE DADOS
     req.connection.query(
-    'INSERT INTO produtos (nome, preco) VALUES (?, ?)', 
-    [req.body.nome, req.body.preco], function (err, resultado)  {
-    
-    if(err) return next(err);
-        return res.json({
-            Mensagem: 'Produto Inserido com Sucesso',
-            ProdutoCriado: produto,
-            id_produto: resultado.insertId
-        })    
+        'INSERT INTO produtos (nome, preco) VALUES (?, ?)', 
+        [
+            req.body.nome,
+            req.body.preco
+        ], 
+            function (err, resultado)
+            {
+            if(err) 
+                return next(err);
+                    return res.json({
+                        Mensagem: 'Produto Inserido com Sucesso',
+                        ProdutoCriado: produto,
+                        id_produto: resultado.insertId
+                    });
     });
-    
-    /*
-    return req.status(201).json({
-		Metodo_POST: 'Produto Inserido com Sucesso',
-		id_produto: resultado.insertId
-    });
-    */
 });
-
 
 /**
  * ROTA PATCH(ALTERAR)
  */
 router.patch('/', (req, res, next) => {
    
-    const produto = {
+    const produto = 
+    {
         nome: req.body.nome,
         preco: req.body.preco
     }
-    // A COXEXÃO É ABERTA ATRAVÉS DA ROTA
-    // ABRIR CONEXÃO COM O BANCO DE DADOS
+
     req.connection.query(
-    'UPDATE produtos SET nome = ?, preco = ? WHERE id_produto = ?', 
-    [req.body.nome, req.body.preco, req.body.id_produto], function (err, resultado)  {
-    
-    if(err) return next(err);
-        return res.json({
-            Mensagem: 'Produto Alterado com Sucesso',
-            ProdutoCriado: produto,
-            id_produto: resultado.insertId
-        })    
+        'UPDATE produtos SET nome = ?, preco = ? WHERE id_produto = ?', 
+            [
+                req.body.nome,
+                req.body.preco, 
+                req.body.id_produto
+            ], 
+                function (err, resultado)  {
+        
+                if(err) 
+                    return next(err);
+                        return res.json({
+                            Mensagem: 'Produto Alterado com Sucesso',
+                            ProdutoCriado: produto,
+                            id_produto: resultado.insertId
+                        })    
     });
-    /*
-    return response.status(201).json({
-        Metodo_PATCH: 'Altera os Produtos'
-    });
-    */
 });
 
 /**
  * ROTA DELETE(DELETAR)
  */
 router.delete('/', (req, res, next) => {
-   
-        // A COXEXÃO É ABERTA ATRAVÉS DA ROTA
-    // ABRIR CONEXÃO COM O BANCO DE DADOS
     req.connection.query(
         'DELETE FROM produtos WHERE id_produto = ?', 
-        [req.body.id_produto], function (err, resultado)  {
+            [
+                req.body.id_produto
+            ], 
+                function (err, resultado)  {
         
-        if(err) return next(err);
-            return res.json({
-                Mensagem: 'Produto Excluido com Sucesso',
-            })    
-        });
-
-    /*
-    return response.status(201).json({
-        Metodo_DELETE: 'Deleta os Produtos'
+                if(err) 
+                    return next(err);
+                        return res.json({
+                        Mensagem: 'Produto Excluido com Sucesso',
+                    })    
     });
-    */
 });
 
 
@@ -111,19 +108,19 @@ router.delete('/', (req, res, next) => {
  * Passando Parametros nas ROTAS para realizar GET/BUSCAS
  */
 router.get('/:id_produto', (req, res, next) =>{
-    //const id = req.params.id_produto;
-
-    // A COXEXÃO É ABERTA ATRAVÉS DA ROTA
-    // ABRIR CONEXÃO COM O BANCO DE DADOS
-    req.connection.query('SELECT * FROM produtos WHERE id_produto = ?;', 
-    [req.params.id_produto], (err, resultado) => {
-        if(err) return next(err);
-        return res.json({
-            produtos: resultado
-        });  
-        // CONEXÃO É DEVOLVIDA AO POOL E ENCERRADA ATRAVES DO MIDDLWARE
-    });
-
+    req.connection.query(
+        'SELECT * FROM produtos WHERE id_produto = ?;', 
+            [
+                req.params.id_produto
+            ], 
+                (err, resultado) => {
+                    if(err)
+                        return next(err);
+                            return res.json({
+                            produtos: resultado
+                         });  
+                         
+    }); 
 });
 
 module.exports = router;
