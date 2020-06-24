9bf4ac518b7653c5eb90c9f73f6b6b8d6eac1583
VitorBassDev <vittorbassdev@gmail.com>
2020-06-24 17:51:08 -0300

    Configuração de Conexão com o Banco de Dados | Middleware Crud na tabela Produtos

--- routes/produtos.js
+++ routes/produtos.js
@@ -26,7 +26,7 @@ router.get('/', (req, res, next) => {
  * ROTA POST(INSERIR)
  */
 router.post('/', (req , res, next) =>{
-    const produdo = {
+    const produto = {
         nome: req.body.nome,
         preco: req.body.preco
     }
@@ -39,7 +39,7 @@ router.post('/', (req , res, next) =>{
     if(err) return next(err);
         return res.json({
             Mensagem: 'Produto Inserido com Sucesso',
-            ProdutoCriado: produdo,
+            ProdutoCriado: produto,
             id_produto: resultado.insertId
         })    
     });
@@ -56,38 +56,74 @@ router.post('/', (req , res, next) =>{
 /**
  * ROTA PATCH(ALTERAR)
  */
-router.patch('/', (request, response, next) => {
+router.patch('/', (req, res, next) => {
+   
+    const produto = {
+        nome: req.body.nome,
+        preco: req.body.preco
+    }
+    // A COXEXÃO É ABERTA ATRAVÉS DA ROTA
+    // ABRIR CONEXÃO COM O BANCO DE DADOS
+    req.connection.query(
+    'UPDATE produtos SET nome = ?, preco = ? WHERE id_produto = ?', 
+    [req.body.nome, req.body.preco, req.body.id_produto], function (err, resultado)  {
+    
+    if(err) return next(err);
+        return res.json({
+            Mensagem: 'Produto Alterado com Sucesso',
+            ProdutoCriado: produto,
+            id_produto: resultado.insertId
+        })    
+    });
+    /*
     return response.status(201).json({
         Metodo_PATCH: 'Altera os Produtos'
     });
+    */
 });
 
 /**
  * ROTA DELETE(DELETAR)
  */
-router.delete('/', (request, response, next) => {
+router.delete('/', (req, res, next) => {
+   
+        // A COXEXÃO É ABERTA ATRAVÉS DA ROTA
+    // ABRIR CONEXÃO COM O BANCO DE DADOS
+    req.connection.query(
+        'DELETE FROM produtos WHERE id_produto = ?', 
+        [req.body.id_produto], function (err, resultado)  {
+        
+        if(err) return next(err);
+            return res.json({
+                Mensagem: 'Produto Excluido com Sucesso',
+            })    
+        });
+
+    /*
     return response.status(201).json({
         Metodo_DELETE: 'Deleta os Produtos'
     });
+    */
 });
 
 
 /**
  * Passando Parametros nas ROTAS para realizar GET/BUSCAS
  */
-router.get('/:id_produto', (request, response, next) =>{
-    const id = request.params.id_produto;
-    
-    if(id == 'especial'){
-        return response.status(200).json({
-            Metodo_GET_PARAMETRO: 'Usando a Rota POST dentro da Rota de Produtos com Parametro',
-            id: id
-        });
-    } else{
-        return response.status(200).json({
-            Metodo_GET_PARAMETRO: 'ID não encontrado '
-        });
-    }
+router.get('/:id_produto', (req, res, next) =>{
+    //const id = req.params.id_produto;
+
+    // A COXEXÃO É ABERTA ATRAVÉS DA ROTA
+    // ABRIR CONEXÃO COM O BANCO DE DADOS
+    req.connection.query('SELECT * FROM produtos WHERE id_produto = ?;', 
+    [req.params.id_produto], (err, resultado) => {
+        if(err) return next(err);
+        return res.json({
+            produtos: resultado
+        });  
+        // CONEXÃO É DEVOLVIDA AO POOL E ENCERRADA ATRAVES DO MIDDLWARE
+    });
+
 });
 
 module.exports = router;
